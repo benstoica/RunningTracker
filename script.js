b403db62f8ad4555b64558f7cc7777216335d649
Ben Stoica <bennystoica@gmail.com>
2020-09-05 20:59:41 -0400

    calculated total & average and updated DOM

--- script.js
+++ script.js
@@ -5,11 +5,25 @@ const entriesWrapper = document.querySelector('#entries');
 function addNewEntry(newEntry) {
     entriesWrapper.removeChild(entriesWrapper.firstElementChild);
     const listItem = document.createElement('li');
-    const listValue = document.createTextNode(newEntry);
+    const listValue = document.createTextNode(newEntry.toFixed(1));
     listItem.appendChild(listValue);
 
     entriesWrapper.appendChild(listItem);
+}
 
+function reducer(total, currentValue) {
+    return total + currentValue;
+}
+
+function calcTotal() {
+    const totalValue = entries.reduce(reducer).toFixed(1);
+    document.getElementById('total').innerText = totalValue;
+    document.getElementById('progressTotal').innerText = totalValue;
+}
+
+function calcAvg() {
+    const avg = (entries.reduce(reducer) / entries.length).toFixed(1);
+    document.getElementById('average').innerText = avg;
 }
 
 function handleSubmit(e) {
@@ -18,7 +32,10 @@ function handleSubmit(e) {
     if(!entry) return;
     document.querySelector('form').reset();
     entries.push(entry);
+
     addNewEntry(entry);
+    calcTotal();
+    calcAvg();
 }
 
 
